c6071685d91813349e773d2fb7eea64b5645a7d9
Juliana Bonardi <106901927+Juh-B@users.noreply.github.com>
2025-03-10 10:54:16 -0300

    Finished 3

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:22:37 by jcosta-b          #+#    #+#             */
-/*   Updated: 2025/01/29 15:23:50 by jcosta-b         ###   ########.fr       */
+/*   Updated: 2025/02/26 11:39:59 by jcosta-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ typedef struct s_stack
 	struct s_stack	*next;
 }	t_stack;
 
-int	verif_args(int argc, char **argv, t_stack **stack_a);
-int	verif_stack(int argc, char **argv, t_stack **stack_a);
-int	verif_order(t_stack *stack);
+int		verif_args(int argc, char **argv, t_stack **stack_a);
+int		verif_stack(int argc, char **argv, t_stack **stack_a);
+int		verif_order(t_stack *stack);
 
-int get_command(t_stack **stack_a, t_stack **stack_b);
+int		get_command(t_stack **stack_a, t_stack **stack_b);
 void	create_stack(t_stack **stack, void *content);
 t_stack	*ft_lst_last(t_stack *stack);
 
